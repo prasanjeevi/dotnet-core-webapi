c27bb41b7a4e9f63841028adfe76ed124cf3c982
prasanjeevi <prasanjeevi@gmail.com>
2018-09-25 10:51:06 +0530

    Replace hardcode with static array in ValueController

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -10,36 +10,41 @@ namespace dotnet_core_webapi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        static List<string> values = new List<string>();
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return values;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return values[id];
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            values.Add(value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            values[id] = value;
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            values.RemoveAt(id);
         }
     }
 }
